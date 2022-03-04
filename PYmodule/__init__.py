PYmodule: kernelS_MBH_M: d_fit=0 factor 2 corrected; Phi_easy: Chi2_M+Chi2_L, same as in MCMC posterior; MLF1p, PYmodule/MLF1p: a case of up-to-date MCMC.

PYmodule/__init__.py
@@ -178,7 +178,7 @@ def kernelS_MBH_M(M1, M0, dt, f_duty, l_cut, d_fit):
     if d_fit:
         lbd = (np.log(M1/M0) + (pow(M1/1e7,d_fit)-pow(M0/1e7,d_fit))/d_fit) / ( f_duty*dt/(0.1*10.*t_Edd) )
     else:
-        lbd = np.log(M1/M0)  / ( f_duty*dt/(0.1*10.*t_Edd) )
+        lbd = 2.* np.log(M1/M0)  / ( f_duty*dt/(0.1*10.*t_Edd) )
     return lbd/l_cut
 
 def kernelS_MBH_Mk(M1, M0, dt, f_duty, l_cut, k_fit):

Phi_2d.py
@@ -56,11 +56,11 @@ a_range = [.1]
 # l_range = [.96]
 # a_range = [.2]
 
-t_range = [80*Myr]
+t_range = [10*Myr]
 f_range = [1]
-d_range = [0.]
-l_range = [.7]
-a_range = [.3]
+d_range = [.25]
+l_range = [9.5]
+a_range = [.01]
 
 T = Ts[0][0]
 f_bsm = 1.
@@ -134,13 +134,6 @@ for t_life in t_range:
                     index = np.logical_and(T['M_BH']>1e7, T['M_BH']<1e10)
                     Chi2_M = np.sum(pow(np.log(T['Phi']/T['W10_MF'])[index], 2))/(np.sum(index)-1)
                     off_M = np.max(abs(np.log(T['Phi']/T['W10_MF'])[index]))
-                    
-                    index = np.where(np.logical_and(1e7<M_BH,M_BH<1e10))
-                    xs = M_BH[index]
-                    ys = np.log10( MF(xs)  ) # Willott 2010 30 points as data
-                    y_model = np.log10( (dn_MBH/dlog10M) [index] )
-                    y_err = 1.
-                    Chi2_M =  np.sum( pow((ys - y_model)/y_err, 2))
 
                 # # --------- Luminosity Function ---------
                     Phi = np.zeros(N_lf)
@@ -185,5 +178,5 @@ for t_life in t_range:
                     # exit(0)
 # print(i)
 if find_min:
-    print('log_prob=',-.5*(Chi2_min*(len(Phi_obs)-1)+Chi2_M))
+    print('log_prob=',-.5*Chi2_min*(len(Phi_obs)-1))
     #, LFname_min,'Chi2_min',Chi2_min, 'Chi2_M',Chi2_M, 'off_L',off_L, 'off_M',off_M)