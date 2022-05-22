Phi_M0,M0_evol: M0 evolve calibration; in Phi_M0, DT corrected in T_seed criterion, not corrected elsewhere; found finer abin_mf bins (500 bins)+ 3 points averaging dP_MBH useful; Phi_easy, Mdist_evol: seeds in Mh=1e11, evolve calibration; in Phi_easy, output refined; MLF4p_logd: for easycali; PY/init: abin_mf 100-> 300 bins (relaxed for higher t_life); PY/MLF4p_logd, models_logd: lambda0->x0; 3 points averaging dP_MBH

M0_evol.py
@@ -1,26 +1,28 @@
 from PYmodule import *
 from PYmodule.l_intg import *
 from datetime import datetime
-# inherited from sk1:BHMF_prev/plotBHMF.py, dist_grow.py;
-# plot histogram version of seed BHMF, after grow, Willott2010 curve.
+# histogram version of fixing M0, t0: BHMF, QLF, ERDF after growth, Willott2010 curve.
 t1 = time.time()
 
 z1 = 6
 t_end = t_from_z(z1)
+t_end = 300*Myr
 
 T = Ts[0][0]
+T['t_col'] = 20*Myr*np.ones(len(T))
+T['Mstar0'] = 1000
+
 Nsite = len(T)
+N_concatenate = int(3e0) # Nsite * N_concatenate samples
+N = Nsite*N_concatenate
 
 f_bsm = 1.
 n_base = n_base[0]
-N_concatenate = int(1e1) # Nsite * N_concatenate samples
-N = Nsite*N_concatenate
 
-# 不同f_seed 不影响BH growth 只是参数不同
-# nbase1e-03,4pr8 nsteps=2e4 的 best fits
-# 用f_seed=0.01 相当于1e2*N_BH sample
-t_life, logd_fit, l_cut, a = 21.8, -3., 0.88, 0.19 # f_seed = 0.01
-t_life, logd_fit, l_cut, a = 21.4, -3, .89, .15 # f_seed = 0.1
+logd_fit = -3
+l_cut = 1.
+a = .5
+t_life = 5
 
 d_fit = pow(10.,logd_fit)
 t_life *= Myr
@@ -28,7 +30,7 @@ t_life *= Myr
 # table stores the cdf of lambda
 x0 = lambda_0/l_cut
 I_toinf = integral_toinf(a,x0)
-x = np.logspace(np.log10(x0),1.2,num=200)
+x = np.logspace(np.log10(x0),3.,num=500)
 Pa_x = integral(a,x,x0)/I_toinf
 
 Nt = int(np.max((t_end-T['t_col'])//t_life)+1)
@@ -49,6 +51,7 @@ with open(Mfname,'w') as fM, open(z6fname, 'w') as fz6:
     for i_concatenate in range(N_concatenate):
         M0 = T['Mstar0']; t = T['t_col']; 
         M1s = [M0]; ts =  [t/Myr]
+        DT = 0
         for i_ in range(Nt):
             if np.any(t + t_life > t_end): 
                 dt = t_life * np.ones(Nsite)
@@ -57,26 +60,31 @@ with open(Mfname,'w') as fM, open(z6fname, 'w') as fz6:
             else:
                 t = t + t_life
                 dt = t_life
+            DT += dt
             uniform_a = np.random.uniform(size=Nsite)
             ls = np.zeros(Nsite)
             for i in range(Nsite):
                 ls[i] = x[np.argmax(Pa_x>uniform_a[i])]
             ls = ls*l_cut
+            # ls[ls>5] =5 # shift of BHMF 并非tail fluctuation引起
 
             M1 = M1M0_d(M0,ls,dt,d_fit)
             # M1 = M1M0_e(M0,dt,ls) #试了 MF还是对不上
+            # print('solution?',np.allclose(ls, .5*(np.log(M1/M0) + (pow(M1/M_cut,d_fit)-pow(M0/M_cut,d_fit))/d_fit) / ( 1.*dt/(0.1*t_Edd) )))
 
             M0 = M1
             L1 = L_M(M1,ls)
             M1450_1 = M1450_Lbol(L1)
             # M1s.append(M1); ts.append(t/Myr)
             # # N_bri.append(len(np.where(L1>=L_bright)[0]))
-        print(np.allclose(t_end,t))
         # M1s = np.array(M1s)
         # M_low = 1e8
         # index = np.where(M1>M_low)
         # np.savetxt(fM, M1s.transpose()[index], fmt='%10.3e')
         np.savetxt(fz6, np.array([M1,ls,L1,M1450_1]).transpose(), fmt='%10.3e')
+
+print(np.allclose(t_end,t))
+print("DT=",DT/Myr)
 
 pyname = sys.argv[0]
 print(pyname,' time: ',time.time()-t1)
@@ -91,7 +99,7 @@ M1, ls, L1, M1450_1 = T_z6['M1'], T_z6['ls'], T_z6['L1'], T_z6['M1450_1']
 hist, bin_edges = np.histogram(M1,bins=abin_mf,density=False)
 hist = hist*n_base*f_bsm*f_seed/N/dlog10M
 ascii.write(Table([np.log10(M_BH),hist,MF(M_BH)]), 
-z6datapre+'Mdist_evolMFz6.txt',
+z6datapre+'M0_evolMFz6.txt',
 names=['M_BH','hist','W10'],
 formats={'M_BH':'10.2e','hist':'10.2e','W10':'10.2e'},
 overwrite=True)
@@ -101,14 +109,13 @@ hist, bin_edges = np.histogram(M1450_1,bins=bin_edg,density=False)
 hist = hist*n_base*f_bsm*f_seed/N/bin_wid*1e9
 hist_DO = hist/corr_U14D20(bin_cen)
 ascii.write(Table([bin_cen,Phi_obs,hist_DO,hist]), 
-z6datapre+'Mdist_evolLFz6.txt',
+z6datapre+'M0_evolLFz6.txt',
 names=['M1450','Phi_obs','hist_DO','hist'],
 formats={'M1450':'10.2f','hist':'10.2e','hist_DO':'10.2e','Phi_obs':'10.2e'},
 overwrite=True)
 
-exit(0)
 # write lambda histogram: lbin, hist_tot, hist_L46, Pana
-lbin = np.linspace(-2,1.2,num=20)
+lbin = np.arange(-3,3,0.1)
 hist, bin_edges = np.histogram(np.log10(ls),bins=lbin,density=False)
 x = np.logspace(lbin[0]-np.log10(l_cut),lbin[-1]-np.log10(l_cut),num=len(lbin)) # for Pana
 Pana = integral(a,x,x0)/I_toinf
@@ -123,7 +130,8 @@ hist_, bin_edges = np.histogram(np.log10(ls_),bins=lbin,density=False)
 strhist_ = 'hist_L%d'%(int(np.log10(L_limit)))
 # lambda histogram file
 ascii.write(Table([bin_edges[:-1],hist_/len(ls),hist/len(ls),Pana[1:]-Pana[:-1]]), 
-prex+strhist_+'.txt',
+# prex+strhist_+'.txt',
+z6datapre+'M0_evolERDFz6.txt',
 names=['log_l',strhist_,'hist_tot','ana'],
 formats={'log_l':'10.2f',strhist_:'10.2e','hist_tot':'10.2e','ana':'10.2e'},
 overwrite=True)

MLF4p_logd.py
@@ -25,15 +25,20 @@ t_life, logd_fit, l_cut, a = 30, -2, 1., 0.1 # f_seed = .01
 t_life, logd_fit, l_cut, a = 35, -2, 1., -0.05 # f_seed = .1
 t_life, logd_fit, l_cut, a = 40, -2, .9, -.2 # f_seed = 1.
 
+# easycali
+t_life, logd_fit, l_cut, a = 21.8, -1, .88, .19 # f_seed = 0.01
+t_life, logd_fit, l_cut, a = 21.4, -3, .89, .15 # f_seed = 0.1
+t_life, logd_fit, l_cut, a = 22.2, -2.98, .99, -.04 # f_seed = 1
+
 
 initial = np.array([t_life,logd_fit,l_cut,a])
 
 ndim = len(initial)
 nwalkers = 100
-nsteps = 10000
+nsteps = 5000
 rball = 1e-4
 
-prex='../4p/nbase{0:.0e}_logd_4pr8_f{1:d}'.format(n_base,int(abs(np.log10(f_seed))))
+prex='../4p/easycali_logd_4pr8_f{1:d}'.format(n_base,int(abs(np.log10(f_seed))))
 # LFbin, LFcur, MF1e8 
 
 fname =prex+'.h5'
@@ -89,9 +94,11 @@ plt.savefig(prex+'_chain.png')
 samples = sampler.flatchain
 probs = sampler.flatlnprobability
 theta_max = samples[np.argmax(probs)]
-print('initial paras: t_life={0:.1e}, logd_fit={1:.1e}, l_cut={2:.1f}, a={3:.1f}, f_seed{4:.0e}, prob{5:.1e}'.format(t_life,logd_fit,l_cut,a,f_seed,probs[0]))
+print('initial paras: t_life={0:.2e}, logd_fit={1:.2e}, l_cut={2:.2f}, a={3:.2f}, f_seed{4:.0e}, prob{5:.2e}'.format(t_life,logd_fit,l_cut,a,f_seed,probs[0]))
 print('Gaussian scatter sigma_l,sigma_a:',sigma_l,sigma_a)
 print('best paras:',labels,theta_max,np.max(probs))
+# print(lnlike(initial),lnprobab(initial));# exit(0)
+# exit(0)
 
 all_samples = np.concatenate(
     (samples, probs[:, None]), axis=1

M_evol.py
@@ -72,17 +72,16 @@ print('np.max(L1)=%.1e'%np.max(L1))
 print('np.max(M1)=%.1e'%np.max(M1))
 
 prex = z6datapre+'/f{0:d}N{1:d}'.format(abs(int(np.log10(f_seed))),int(np.log10(N_BH)))
-prex = '../'+datetime.now().strftime('%Y-%m-%d_%H-%M_')
-# prex = '../f{0:d}N{1:d}'.format(abs(int(np.log10(f_seed))),int(np.log10(N_BH)))
+prex = '../4pevol/'+datetime.now().strftime('%m%d%H%M_')
 
 # z=6 BH mass, λ, L_bol
-ascii.write(Table([M1, ls, L1]),prex+'BHatz6.dat',
+ascii.write(Table([M1, ls, L1]),prex+'BHatz6.txt',
 names=['M1','ls','L1'],formats={'M1':'10.2e','ls':'10.2e','L1':'10.2e'},overwrite=True)
 
 # all time samples
 M1s = np.array(M1s)
 L1s = np.array(L1s)
-ascii.write(Table([zs,ts,N_bri]),prex+'tN_evol.dat',names=['zs','ts','N_bri'],
+ascii.write(Table([zs,ts,N_bri]),prex+'tN_evol.txt',names=['zs','ts','N_bri'],
 formats={'zs':'10.2f','ts':'10.2e','N_bri':'10.2e'},overwrite=True)
 
 # BH mass evol. M growth tracks above M_low
@@ -91,7 +90,7 @@ index = np.where(M1>M_low)
 M1s = M1s.transpose()[index]
 L1s = L1s.transpose()[index]
 print('M1s.shape:',M1s.shape)
-np.savetxt(prex+'Mevol.dat', M1s,fmt='%10.3e')
+np.savetxt(prex+'Mevol.txt', M1s,fmt='%10.3e')
 
 # M1450=-26, Lbol=1e47; M1450=-22, Lbol=2e45
 L_limit = 1e46
@@ -112,7 +111,7 @@ hist_, bin_edges = np.histogram(np.log10(ls_),bins=lbin,density=False)
 strhist_ = 'hist_L%d'%(int(np.log10(L_limit)))
 # lambda histogram file
 ascii.write(Table([bin_edges[:-1],hist_/len(ls),hist/len(ls),Pana[1:]-Pana[:-1]]), 
-prex+strhist_+'.dat',
+prex+strhist_+'.txt',
 names=['log_l',strhist_,'hist_tot','ana'],
 formats={'log_l':'10.2f',strhist_:'10.2e','hist_tot':'10.2e','ana':'10.2e'},
 overwrite=True)

PYmodule/__init__.py
@@ -34,7 +34,7 @@ a = -.7
 
 lambda_0 = 0.01 # starting point of lambda; x0 now changable
 x0 = 0.01 # integration of l/l_cut, starting point; previous context
-f_seed = .01
+f_seed = 1
 
 l_mean, a_mean = 0.6, 0.
 sigma_l, sigma_a = .4, .3
@@ -164,8 +164,8 @@ def M1M0_d(M0, l, dt, delta, M_cut = M_cut):
 
         if np.all(abs(dM1) < .01*M1):
             break
-        if i>5:
-            print('i: %d too much interation'%i)
+        # if i>5:
+        #     print('i: %d too much interation'%i)
     if i==9:
         print('not converge...')
     # print('final match?',np.log(M1/M0) + (pow(M1, delta)-pow(M0, delta))/delta - l*dt/eta_0/t_Edd )
@@ -226,9 +226,9 @@ def kernelS_MBH(Mgrow_ratio, dt, f_duty, l_cut):
 def kernelS_MBH_M(M1, M0, dt, f_duty, l_cut, d_fit, logM_0=logM0):
     M_cut = pow(10., logM_0)
     if d_fit:
-        lbd = (np.log(M1/M0) + (pow(M1/M_cut,d_fit)-pow(M0/M_cut,d_fit))/d_fit) / ( f_duty*dt/(0.1*t_Edd) )
+        lbd = .5*(np.log(M1/M0) + (pow(M1/M_cut,d_fit)-pow(M0/M_cut,d_fit))/d_fit) / ( f_duty*dt/(0.1*t_Edd) )
     else:
-        lbd = 2.* np.log(M1/M0)  / ( f_duty*dt/(0.1*t_Edd) )
+        lbd = np.log(M1/M0)  / ( f_duty*dt/(0.1*t_Edd) )
     return lbd/l_cut
 
 def kernelS_MBH_M_mesh(M1, M0, dt, f_duty, l_cut, d_fit, logM_0=logM0):
@@ -500,7 +500,7 @@ for iM in range(N_Mh):
     Ts.append(TM)
 
 # MF bins
-abin_mf =  np.logspace(2,12,num=100) # default endpoint=True
+abin_mf =  np.logspace(2,12,num=300) # default endpoint=True
 M_BH = abin_mf[:-1]*np.sqrt(abin_mf[1]/abin_mf[0])
 bin_left = abin_mf[:-1]; bin_right = abin_mf[1:]
 wid_mf = bin_right - bin_left

PYmodule/models_logd.py
@@ -16,8 +16,8 @@ def model(theta, z = int(6), f_seed=f_seed, l_cut= l_cut, a=a):
     t_life, logd_fit, l_cut, a = theta
     d_fit = pow(10.,logd_fit)
     t_life = t_life * Myr # wli: not *=!!!!!! theta changed by this
-    I_toinf = integral_toinf(a,lambda_0/l_cut)
-
+    x0 = lambda_0/l_cut
+    I_toinf = integral_toinf(a,x0)
 ## --------- Mass Function ---------
     tz = t_from_z(z)
     dn_MBH = np.zeros(N_mf)
@@ -33,19 +33,30 @@ def model(theta, z = int(6), f_seed=f_seed, l_cut= l_cut, a=a):
         # new seeds (using 2d meshgrids)
         if len(T_seed):
             # z_mesh = kernelS_MBHmesh(abin_mf, T_seed['Mstar0'], dt_seed, l_cut)
-            z_mesh = kernelS_MBH_M_mesh(abin_mf, T_seed['Mstar0'], dt_seed, 1., l_cut, d_fit)*l_cut
-            z_mesh[z_mesh<lambda_0] = lambda_0
-            Ps = integral(a,z_mesh/l_cut,lambda_0/l_cut)/I_toinf
+            z_mesh = kernelS_MBH_M_mesh(abin_mf, T_seed['Mstar0'], dt_seed, 1., l_cut, d_fit)
+            z_mesh[z_mesh<x0] = x0
+            Ps = integral(a,z_mesh,x0)/I_toinf
             dP_seed = Ps[1:,:] - Ps[:-1,:]
             dP_seed = np.nansum(dP_seed, axis=1)/len(T)
         else:
             dP_seed = 0.
         # prev BHMF
         # z_mesh = kernelS_MBHmesh(M_BH, abin_mf, t_life, l_cut)
-        z_mesh = kernelS_MBH_M_mesh(M_BH, abin_mf, t_life, 1., l_cut, d_fit)*l_cut
-        z_mesh[z_mesh<lambda_0] = lambda_0
-        Ps = integral(a,z_mesh/l_cut,lambda_0/l_cut)/I_toinf
+        z_mesh = kernelS_MBH_M_mesh(M_BH, abin_mf, t_life, 1., l_cut, d_fit)
+        z_mesh[z_mesh<x0] = x0
+        Ps = integral(a,z_mesh,x0)/I_toinf
         dP_MBH = np.nansum( (Ps[:,:-1]-Ps[:,1:])*dP_MBH_prev, axis=1) + dP_seed
+
+        z_mesh_left = kernelS_MBH_M_mesh(bin_left, abin_mf, t_life, 1., l_cut, d_fit)
+        z_mesh_left[z_mesh_left<x0] = x0
+        Ps = integral(a,z_mesh_left,x0)/I_toinf
+        dP_MBH_left = np.nansum( (Ps[:,:-1]-Ps[:,1:])*dP_MBH_prev, axis=1) + dP_seed
+        z_mesh_right = kernelS_MBH_M_mesh(bin_right, abin_mf, t_life, 1., l_cut, d_fit)
+        z_mesh_right[z_mesh_right<x0] = x0
+        Ps = integral(a,z_mesh_right,x0)/I_toinf
+        dP_MBH_right = np.nansum( (Ps[:,:-1]-Ps[:,1:])*dP_MBH_prev, axis=1) + dP_seed
+        
+        dP_MBH = (dP_MBH+dP_MBH_left+dP_MBH_right)/3.
 
         Nt -= 1
 
@@ -65,9 +76,9 @@ def model(theta, z = int(6), f_seed=f_seed, l_cut= l_cut, a=a):
     Chi2_M =  np.sum( pow((ys - y_model)/mf_err, 2))
 
 # # --------- Luminosity Function ---------
-    z_mesh = kernelS_M1450_mesh(bin_edg, M_BH, l_cut)*l_cut
-    z_mesh[z_mesh<lambda_0] = lambda_0
-    Ps = integral(a,z_mesh/l_cut,lambda_0/l_cut)/I_toinf
+    z_mesh = kernelS_M1450_mesh(bin_edg, M_BH, l_cut)
+    z_mesh[z_mesh<x0] = x0
+    Ps = integral(a,z_mesh,x0)/I_toinf
     dPhi_mesh = np.nansum((Ps[:-1,:]-Ps[1:,:])*dn_MBH,axis=1)
     Phi = dPhi_mesh/bin_wid
     Phi *= 1e9

Phi_M0.py
@@ -8,31 +8,12 @@ Ntr = 10000
 eta = 0.3
 z = int(6)
 tz = t_from_z(z)
-alpha = 1.
+tz = 300*Myr
 
-# LF bins same w/ Matsu18
-N_lf = len(bin_cen)
-
-# # initial: lambda_0=0.01, logM0 = 8.
-# t_life, d_fit, l_cut, a = 20, .01, 1., 0.1 # f_seed = .01, log_prob= -9.89
-# t_life, d_fit, l_cut, a = 25, .01, 1.2, -0.2 # f_seed = .1, log_prob= -36.82
-# t_life, d_fit, l_cut, a = 30, .01, 1., -.2 # f_seed = 1., log_prob= -13.88
-# # # best:
-# # t_life, d_fit, l_cut, a = 19.8, 1.2e-3, 1.1557, -1.8e-01 # f_seed = 1.
-
-# new_nbase initial: lambda_0=0.01, logM0 = 8.
-t_life, d_fit, l_cut, a = 30, .01, 1., 0.1 # f_seed = .01, log_prob= -9.11
-t_life, d_fit, l_cut, a = 35, .01, 1.2, -0.2 # f_seed = .1, log_prob= -16.37
-t_life, d_fit, l_cut, a = 40, .01, .9, -.2 # f_seed = 1., log_prob= -11.45
-
-t_life, d_fit, l_cut, a = 21.4, 0., .89, .15 # f_seed = 0.1, M1M0_e
-t_life, d_fit, l_cut, a = 21.4, 0.001, .89, .15 # f_seed = 0.1, M1M0_d
-
-# after more abin_mf bins, after calibration w/ direct sampling; 
-# easycali initial: 
-t_life, logd_fit, l_cut, a = 21.8, -1, .88, .19 # f_seed = 0.01
-t_life, logd_fit, l_cut, a = 21.4, -3, .89, .15 # f_seed = 0.1
-t_life, logd_fit, l_cut, a = 22.2, -2.98, .99, -.04 # f_seed = 1
+d_fit = 1e-3
+l_cut = 1.
+a = .5
+t_life = 5
 
 
 x0 = lambda_0/l_cut
@@ -41,9 +22,10 @@ I_toinf = integral_toinf(a,x0)
 print('t_life, d_fit, l_cut, a,  f_seed, x0, logM0 = ', 
 t_life,', ',d_fit,', ', l_cut,', ',a,', ', f_seed,', ', x0,', ', logM0,', ')
 
-d_fit = pow(10.,logd_fit)
 t_life *= Myr
 T = Ts[0][0]
+T['t_col'] = 20*Myr*np.ones(len(T))
+T['Mstar0'] = 1000
 f_bsm = 1.
 n_base = n_base[0]
 
@@ -58,9 +40,10 @@ Nt = np.max((tz-T['t_col'])//t_life)
 Nmax = Nt
 dP_MBH = np.zeros(N_mf)
 # t_tot = np.zeros(len(T))
+DT = 0
 while Nt>=0:
     t_point = tz - Nt*t_life
-    T_seed = T[np.logical_and(t_point-t_life<=T['t_col'],T['t_col']<t_point)]
+    T_seed = T[np.logical_and(t_point-t_life<T['t_col'],T['t_col']<=t_point)]
     dt_seed = t_point - T_seed['t_col']
     dP_MBH_prev = dP_MBH.copy()
     # new seeds (using 2d meshgrids)
@@ -71,10 +54,12 @@ while Nt>=0:
         Ps = integral(a,z_mesh,x0)/I_toinf
         dP_seed = Ps[1:,:] - Ps[:-1,:]
         dP_seed = np.nansum(dP_seed, axis=1)/len(T)
+        DT+=dt_seed
     else:
         dP_seed = 0.
     # prev BHMF
     # z_mesh = kernelS_MBHmesh(M_BH, abin_mf, t_life, l_cut)
+    DT+= t_life
     z_mesh = kernelS_MBH_M_mesh(M_BH, abin_mf, t_life, 1., l_cut, d_fit)
     z_mesh[z_mesh<x0] = x0
     Ps = integral(a,z_mesh,x0)/I_toinf
@@ -88,11 +73,11 @@ while Nt>=0:
     z_mesh_right[z_mesh_right<x0] = x0
     Ps = integral(a,z_mesh_right,x0)/I_toinf
     dP_MBH_right = np.nansum( (Ps[:,:-1]-Ps[:,1:])*dP_MBH_prev, axis=1) + dP_seed
-    
+    # 3 points averaging; tried 5 points -> dP_MBH, no use
     dP_MBH = (dP_MBH+dP_MBH_left+dP_MBH_right)/3.
 
     Nt -= 1
-
+print('DT=',np.mean((DT-t_life)/Myr))
 dn_MBH = dP_MBH*n_base*f_bsm*f_seed
 
 consv_ratio = np.nansum(dn_MBH)/(n_base*f_seed)
@@ -105,7 +90,7 @@ T = Table(
     names=('M_BH','Phi','W10_MF')
 )
 
-MFname = z6datapre+'Phi_easyMF'
+MFname = z6datapre+'Phi_M0MF'
 ascii.write( Table([np.log10(T['M_BH']), T['Phi'], T['W10_MF']],
             names=['M_BH','Phi','W10_MF']),
             MFname,
@@ -125,6 +110,15 @@ y_model = np.log10( (dn_MBH/dlog10M)[index][::len(index[0])//10] )
 y_err = pow(np.log10(xs)-8.5,2)/3. + .2 # from 0.2 to 0.95
 Chi2_M =  np.sum( pow((ys - y_model)/y_err, 2))
 
+lbin = np.arange(-3,3,0.1)
+x = np.logspace(lbin[0]-np.log10(l_cut),lbin[-1]-np.log10(l_cut),num=len(lbin)) # for Pana
+Pana = integral(a,x,x0)/I_toinf
+with open(z6datapre+"Phi_M0ERDFz6.txt",'w') as f:
+    np.savetxt(f, np.array([lbin[:-1],Pana[1:]-Pana[:-1]]).transpose(), fmt='%10.3e')
+
+print('time=',time.time()-t1)
+exit(0)
+
 # # --------- Luminosity Function ---------
 z_mesh = kernelS_M1450_mesh(bin_edg, M_BH, l_cut)
 z_mesh[z_mesh<x0] = x0
@@ -143,11 +137,11 @@ Chi2 = np.nansum(pow( (np.log(Phi_DO) - np.log(Phi_obs))/np.log(Phi_err), 2))/(l
 off_L = np.nanmax(abs( (np.log(Phi_DO) - np.log(Phi_obs))/np.log(Phi_err)))
 
 T = Table(
-    [bin_cen,Phi_obs,Phi_DO,Phi,Chi2*np.ones(N_lf)],
-    names=('bin_cen','Phi_obs','Phi_DO','Phi','Chi2')
+    [bin_cen,Phi_obs,Phi_DO,Phi],
+    names=('bin_cen','Phi_obs','Phi_DO','Phi')
 )
 
-LFname = z6datapre+'Phi_easyLF'
+LFname = z6datapre+'LFIMF_easy_newnbase'
 ascii.write(T, LFname,
             formats={'bin_cen':'6.2f','Phi_obs':'4.2e','Phi_DO':'4.2e','Phi':'4.2e','Chi2':'4.2e'},
             overwrite=True)
@@ -162,4 +156,4 @@ print('Chi2_M=',Chi2_M,'Chi2_L=',Chi2)
 print('log_prob=',-.5*(Chi2_min*(len(Phi_obs)-1)+Chi2_M))
 #, LFname_min,'Chi2_min',Chi2_min, 'Chi2_M',Chi2_M, 'off_L',off_L, 'off_M',off_M)
 
-print('time=',time.time()-t1)
+# print('time=',time.time()-t1)

try1.py
@@ -7,9 +7,57 @@ from scipy.stats import norm, uniform
 # N1 = 1; N2 = 2; N3 = 3; N4 = 4; N5 = 5
 # a = np.ones((N1,N2,N3,N4,N5))
 
+from PYmodule.MLF4p_logd import *
+from PYmodule.models_logd import *
+t_life, d_fit, l_cut, a = 21.4, 0.001, .89, .15 # f_seed = 0.1, M1M0_d
+logd_fit = np.log10(d_fit)
+
+x = (t_life, logd_fit, l_cut, a)
+print('MLF4p_logd',lnlike(x))
+print('MLF4p_logd',lnprobab(x))
+print('modles_logd',-.5*model(x)['Chi2_M'] - .5*model(x)['Chi2_L'])
+exit(0)
+
+a = np.arange(0,6,1)
+a = a.reshape(2,3)
+print(a)
+print(a*np.array([1,2,3]))
+print(np.sum(a*np.array([1,2,3]),axis=1))
+exit(0)
+
+abin_mf = np.logspace(1,3,num=3,base=np.e)
+bin_left = abin_mf[:-1]; bin_right = abin_mf[1:]
+M_BH = abin_mf[:-1]*np.sqrt(abin_mf[1]/abin_mf[0])
+n_mf = np.ones(len(M_BH))
+
+dt = .1*t_Edd
+l_cut = 1.; d_fit=0.
+z_mesh0 = kernelS_MBH_M_mesh(abin_mf, abin_mf, dt, 1., l_cut, d_fit)
+z_mesh1 = kernelS_MBH_M_mesh(bin_left, abin_mf, dt, 1., l_cut, d_fit)
+z_mesh2 = kernelS_MBH_M_mesh(M_BH, abin_mf, dt, 1., l_cut, d_fit)
+z_mesh3 = kernelS_MBH_M_mesh(bin_right, abin_mf, dt, 1., l_cut, d_fit)
+
+z_mesh = z_mesh0
+z_mesh = (z_mesh[:,:-1]+z_mesh[:,1:])/2.
+print(z_mesh3)
+print(z_mesh1)
+
+x0 = 0
+
+z_mesh[z_mesh<x0] = x0
+Ps = z_mesh/2
+n_mfgrow = np.nansum( (Ps[:,:-1]-Ps[:,1:])*n_mf, axis=1)
+
+print(np.nansum(n_mfgrow))
+exit(0)
+M1450_1  = np.linspace(-20,-30)
+with open("../4pevol/LF.txt",'w') as f:
+    np.savetxt(f, np.array([M1450_1,LF_M1450(M1450_1)]).transpose(), fmt='%10.3e')
+exit(0)
 # a = np.array([1.2, 2.3, 4.5])
 # with open("test.txt",'w') as f:
-#     for i in range(1):
+#     f.write('{0:10s}{1:10s}{2:10s}\n'.format('M1','ls','L1'))
+#     for i in range(3):
 #         np.savetxt(f, np.array([a,a]).transpose(), fmt='%10.3f')
 # exit(0)
 # test MLF3p lnlike & models same w/ Phi_easy_l.py