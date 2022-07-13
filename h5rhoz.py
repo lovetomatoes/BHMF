390d4ad8323b66d814f270dc92f4d5a76958df69
lovetomatoes <marafor80@gmail.com>
2022-07-13 13:56:16 +0800

    h5rhoz*: ln version

--- h5rhoz.py
+++ h5rhoz.py
@@ -10,11 +10,13 @@ zmax = 11
 M_up  = 1e7
 L_up  = -26
 # L_ups = [-23,-24,-25,-26,-27] # 可用集合
-ndraw = 60
+ndraw = 3
 
 f_seedlabel = 'f%d'%abs(int(np.log10(f_seed)))
-prex = '../4p/M0r8_' + f_seedlabel
-fname = prex + '.h5'
+readprex = '../4p/M0r8_' + f_seedlabel
+readprex = '../M0r8_' + f_seedlabel
+fname = readprex + '.h5'
+prex = readprex + 'ln'
 # fname = '../4p/3n2f_logd_4pr8_f2.h5'
 
 labels = [r'$\mathrm{t_{life}}$', r'$\log \delta$', r'$\lambda_0$', r'$\alpha$']
@@ -44,8 +46,8 @@ figm, axm = plt.subplots(num='MF',figsize=(10, 10))
 figl, axl = plt.subplots(num='LF',figsize=(10, 10))
 curve_name = 'MF'
 zs = np.arange(z,zmax,1)
-rhoM = []; rhoM_med = []; rhoM_std = []
-nL = []; nL_med = []; nL_std = []
+rhoM = []; rhoM_min = []; rhoM_max = []
+nL = []; nL_min = []; nL_max = []
 # xs of models
 Mx = M_BH[::int(N_mf/100)] # ~100 points
 dlog10Mx = np.log10(Mx[1]/Mx[0])
@@ -59,9 +61,14 @@ for z in zs:
         mod = model_thetas[i]['MF'][::int(N_mf/100)]
         mod_list.append(mod)
         # axm.plot(Mx, mod, c='grey',label='_',alpha=.2)
-    spread = np.std(mod_list,axis=0)
-    med_model = np.median(mod_list,axis=0)
-    axm.fill_between(Mx,med_model-spread,med_model+spread,color='C%d'%(z%10),alpha=0.3,label=r'_$1\sigma$ Posterior Spread')
+    lnspread = np.std(np.log(mod_list),axis=0)
+    lnmed = np.median(np.log(mod_list),axis=0)
+    print('where lnspread nan?',Mx[np.isnan(lnspread)])
+    print('where lnmed nan?',Mx[np.isnan(lnmed)])
+    print('where lnspread inf?',Mx[np.isinf(lnspread)])
+    print('where lnmed inf?',Mx[np.isinf(lnmed)])
+
+    axm.fill_between(Mx,np.exp(lnmed-lnspread),np.exp(lnmed+lnspread),color='C%d'%(z%10),alpha=0.3,label=r'_$1\sigma$ Posterior Spread')
     #--- bestfit curve
     best_model = model(theta_max,z,f_seed,corr)
     y_best = best_model['MF'][::int(N_mf/100)]
@@ -69,12 +76,12 @@ for z in zs:
     #--- mass integration above M_up
     dn_best = y_best*dlog10Mx
     index = np.where(Mx>M_up)
-    rhoM.append(np.sum(dn_best[index]*Mx[index]))
-    dn_med = med_model*dlog10Mx
-    rhoM_med.append(np.sum(dn_med[index]*Mx[index]))
-    dn_std = spread*dlog10Mx
-    rhoM_std.append(np.sum(dn_std[index]*Mx[index]))
-    # print(dlog10Mx*np.sum((med_model-spread)[Mx>M_up]),dlog10Mx*np.sum((med_model+spread)[xs>M_up]))
+    rhoM.append(np.nansum(dn_best[index]*Mx[index]))
+    dn_min = np.exp(lnmed-lnspread) *dlog10Mx
+    rhoM_min.append(np.nansum(dn_min[index]*Mx[index]))
+    dn_max = np.exp(lnmed+lnspread) *dlog10Mx
+    rhoM_max.append(np.nansum(dn_max[index]*Mx[index]))
+    # print(dlog10Mx*np.nansum((lnmed-lnspread)[Mx>M_up]),dlog10Mx*np.nansum(np.exp(lnmed+lnspread)[xs>M_up]))
 
     #-------   LF   -------#
     mod_list = []
@@ -82,20 +89,22 @@ for z in zs:
         mod = model_thetas[i]['LF']
         mod_list.append(mod)
         # axl.plot(M1450, mod, c='grey',label='_',alpha=.2)
-    spread = np.std(mod_list,axis=0)
-    med_model = np.median(mod_list,axis=0)
-    axl.fill_between(M1450,med_model-spread,med_model+spread,color='C%d'%(z%10),alpha=0.3,label=r'_$1\sigma$ Posterior Spread')
+    lnspread = np.std(np.log(mod_list),axis=0)
+    lnmed = np.median(np.log(mod_list),axis=0)
+    axl.fill_between(M1450,np.exp(lnmed-lnspread),np.exp(lnmed+lnspread),color='C%d'%(z%10),alpha=0.3,label=r'_$1\sigma$ Posterior Spread')
     #--- bestfit curve
     y_best = best_model['LF']
     axl.plot(M1450, y_best, color='C%d'%(z%10), label='z=%d'%z)
     #--- integration brighter than L_up
     dn_best = y_best*dmag
-    nL.append(np.sum(dn_best[M1450<L_up]))
-    dn_med = med_model*dmag
-    nL_med.append(np.sum(dn_med[M1450<L_up]))
-    dn_std = spread*dmag
-    nL_std.append(np.sum(dn_std[M1450<L_up]))
+    index = np.where(M1450<L_up)
+    nL.append(np.nansum(dn_best[index]))
+    dn_min = np.exp(lnmed-lnspread)*dmag
+    nL_min.append(np.nansum(dn_min[index]))
+    dn_max = np.exp(lnmed+lnspread)*dmag
+    nL_max.append(np.nansum(dn_max[index]))
     print('after z{:d}, running time: {:.1f} min'.format(z,(time.time()-t1)/60))
+# print(nL_min,nL_max)
 
 axl.set_xlim(np.max(M1450),np.min(M1450))
 axl.set_ylim(5e-3,1e2)
@@ -104,7 +113,7 @@ axl.set_yscale('log')
 # axl.set_ylabel(r'$\mathrm{\Phi~(Gpc^{-3}mag^{-1})}$',fontsize=fslabel)
 axl.tick_params(labelsize=fstick)
 # plt.tight_layout()
-figl.savefig(prex+'ndraw{:d}LF_spreadz{:d}.png'.format(ndraw,z),dpi=300,bbox_inches='tight')
+figl.savefig(prex+'ndraw{:d}LF_spread.png'.format(ndraw),dpi=300,bbox_inches='tight')
 
 axm.set_xlim(1e7,1e10); axm.set_xscale('log')
 axm.set_ylim(1e-10,1e-4); axm.set_yscale('log')
@@ -112,24 +121,24 @@ axm.legend(fontsize=fslegend)
 # axm.set_xlabel(r'$\mathrm{M_{BH}~(M_\odot)}$',fontsize=fslabel)
 # axm.set_ylabel(r'$\mathrm{\Phi_M~(Mpc^{-3}dex^{-1})}$',fontsize=fslabel)
 axm.tick_params(labelsize=fstick)
-figm.savefig(prex+'ndraw{:d}MF_spreadz{:d}.png'.format(ndraw,z),dpi=300,bbox_inches='tight')
+figm.savefig(prex+'ndraw{:d}MF_spread.png'.format(ndraw),dpi=300,bbox_inches='tight')
 
-# rhoM_med=rhoM; rhoM_std=rhoM
+# rhoM_min=rhoM; rhoM_max=rhoM
 # fname = prex+'rhoM_evol.txt'
 fname = prex+'ndraw{:d}rhoM_evol.txt'.format(ndraw)
-ascii.write( Table([zs, rhoM, rhoM_med, rhoM_std],
-            names=['zs','rhoM','rhoM_med','rhoM_std']),
+ascii.write( Table([zs, rhoM, rhoM_min, rhoM_max],
+            names=['zs','rhoM','rhoM_min','rhoM_max']),
             fname,
-            formats={'zs':'5d','rhoM':'4.2e','rhoM_med':'4.2e','rhoM_std':'4.2e'},
+            formats={'zs':'5d','rhoM':'4.2e','rhoM_min':'4.2e','rhoM_max':'4.2e'},
             overwrite=True)
 
-# nL_med=nL; nL_std=nL
+# nL_min=nL; nL_max=nL
 # fname = prex+'nL_evol.txt'
 fname = prex+'ndraw{:d}nL_evol.txt'.format(ndraw)
-ascii.write( Table([zs, nL, nL_med, nL_std],
-            names=['zs','nL','nL_med','nL_std']),
+ascii.write( Table([zs, nL, nL_min, nL_max],
+            names=['zs','nL','nL_min','nL_max']),
             fname,
-            formats={'zs':'5d','nL':'4.2e','nL_med':'4.2e','nL_std':'4.2e'},
+            formats={'zs':'5d','nL':'4.2e','nL_min':'4.2e','nL_max':'4.2e'},
             overwrite=True)
 
 # print('time=',time.time()-t1)
--- h5rhozplot.py
+++ h5rhozplot.py
@@ -10,7 +10,8 @@ fMname = prex+'rhoM_evol.txt'# %int(abs(np.log10(f_seed)))
 fLname = prex+'rhoL_evol.txt'
 
 f_seedlabel = 'f%d'%abs(int(np.log10(f_seed)))
-prex = '../4p/M0r8_' + f_seedlabel
+prex = '../4p/M0r8_' + f_seedlabel + 'ln'
+prex = '../M0r8_' + f_seedlabel + 'ln'
 ndraw = 3
 fMname = prex+'ndraw{:d}rhoM_evol.txt'.format(ndraw)
 fLname = prex+'ndraw{:d}nL_evol.txt'.format(ndraw)
@@ -19,10 +20,12 @@ TM = ascii.read(fMname, guess=False, delimiter=' ')
 TL = ascii.read(fLname, guess=False, delimiter=' ')
 
 zs = TM['zs']
-rhoM = TM['rhoM']; rhoM_med = TM['rhoM_med']; rhoM_std = TM['rhoM_std']
-nL = TL['nL']; nL_med = TL['nL_med']; nL_std = TL['nL_std']
+rhoM = TM['rhoM']; rhoM_min = TM['rhoM_min']; rhoM_max = TM['rhoM_max']
+nL = TL['nL']; nL_min = TL['nL_min']; nL_max = TL['nL_max']
 
-figl, axl = plt.subplots(num='LF',figsize=(10, 10))
+# figl, axl = plt.subplots(num='LF',figsize=(10, 10))
+figl = plt.figure(num='LF',figsize=(10, 10))
+axl = figl.add_subplot(1, 1, 1)
 z_ = np.arange(5.6,7.5,.1)
 dash_k = 0.39*pow(10., -.78*(z_-6.7))
 axl.plot(z_,dash_k, '--',c='black')
@@ -33,8 +36,10 @@ axl.errorbar(6.7,0.39,yerr=0.11,fmt='s',elinewidth=2,capsize=4,label='Wang+ 2019
 
 for i in range(len(zs)):
     z = zs[i]
-    axl.errorbar(z,nL[i],yerr=nL_std[i],fmt='o',ms=10,elinewidth=2,capsize=4,
-            c='C%d'%z,label='z=%d'%z,mfc='none')
+    axl.errorbar(z,nL[i],
+        yerr=[[nL[i]-nL_min[i]],[nL_max[i]-nL[i]]],
+        fmt='o',ms=10,elinewidth=2,capsize=4,
+        color='C%d'%(z%10),label='z=%d'%z,mfc='none')
 axl.plot(zs,nL,c='grey')
 axl.set_xlim(5.5,10.5)
 # axl.set_ylim(1e-10,1e-4); 
@@ -43,15 +48,19 @@ axl.legend(fontsize=fslegend)
 # axl.set_xlabel(r'$\mathrm{M_{BH}~(M_\odot)}$',fontsize=fslabel)
 # axl.set_ylabel(r'$\mathrm{\Phi_M~(Mpc^{-3}dex^{-1})}$',fontsize=fslabel)
 axl.tick_params(labelsize=fslabel)
-figl.savefig(prex+'nL.png',dpi=300,bbox_inches='tight')
+figl.savefig(prex+'ndraw{:d}nL.png'.format(ndraw),dpi=300,bbox_inches='tight')
 
 
-figm, axm = plt.subplots(num='MF',figsize=(10, 10))
+# figm, axm = plt.subplots(num='MF',figsize=(10, 10))
+figm = plt.figure(num='MF',figsize=(10, 10))
+axm = figm.add_subplot(1, 1, 1)
 for i in range(len(zs)):
     z = zs[i]
-    axm.errorbar(z, rhoM[i],yerr=rhoM_std[i],fmt='o',elinewidth=2,capsize=4,
-            color='C%d'%z,label='z=%d'%z)
-axl.plot(zs,rhoM,c='grey')
+    axm.errorbar(z, rhoM[i],
+        yerr=[[rhoM[i]-rhoM_min[i]],[rhoM_max[i]-rhoM[i]]],
+        fmt='o',elinewidth=2,capsize=4,
+        color='C%d'%(z%10),label='z=%d'%z)
+# axm.plot(zs,rhoM,c='grey')
 axm.set_xlim(5.5,10.5)
 # axm.set_ylim(1e-10,1e-4); 
 axm.set_yscale('log')
@@ -59,4 +68,4 @@ axm.legend(fontsize=fslegend)
 # axm.set_xlabel(r'$\mathrm{M_{BH}~(M_\odot)}$',fontsize=fslabel)
 # axm.set_ylabel(r'$\mathrm{\Phi_M~(Mpc^{-3}dex^{-1})}$',fontsize=fslabel)
 axm.tick_params(labelsize=fslabel)
-figm.savefig(prex+'rhoM.png',dpi=300,bbox_inches='tight')
+figm.savefig(prex+'ndraw{:d}rhoM.png'.format(ndraw),dpi=300,bbox_inches='tight')
